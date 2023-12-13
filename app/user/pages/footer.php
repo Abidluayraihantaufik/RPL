412a19d7bcb6d083f1f318e9679c9c1f30e98a0b
Abidluayraihantaufik <spaskeren30@gmail.com>
2023-12-13 18:49:45 +0700

    RPL

---
+++ app/user/pages/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+    </div>
+    <strong> <?= date('Y'); ?>. </strong>ABID LUAY RAIHAN TAUFIK.
+    
+</footer>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+//------------------------------::::::::::::::::::::------------------------------\\
+// Dibuat oleh ABID LUAY RAIHAN TAUFIK \\
+//------------------------------::::::::::::::::::::------------------------------\\
+
+// Arahkan user ketika pertama kali membuka applikasi ke halaman login
+header("location: masuk");
